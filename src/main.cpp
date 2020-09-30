83df9c85e2cc779165ab5d0b095d048493b579ec
markoburda <burda@ucu.edu.ua>
2020-09-30 22:18:28 +0300

    cd and fork exec

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
+#include <string>
 #include <boost/program_options.hpp>
-
 #include "operations/operations.hpp"
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define CMD_SIZE 256
+
+int forking(){
+    pid_t parent = getpid();
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        std::cerr << "Failed to fork\n" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    else if (pid > 0)
+    {
+        std::cout << "Parent: " << parent << ", child: " << pid << std::endl;
+        int status;
+        waitpid(pid, &status, 0);
+        std::cout << "child exited with code" << status << std::endl;
+    }
+    else
+    {
+        //TODO
+    }
+    return 0;
+}
+
+int cd(const char* path){
+    if (chdir(path) != 0)
+        std::cerr << "cd: " << path << ": No such file or directory" << std::endl;
+
+    return 0;
+}
 
 int main(int argc, char **argv) {
-    int variable_a, variable_b;
 
-    namespace po = boost::program_options;
-
-    po::options_description visible("Supported options");
-    visible.add_options()
-            ("help,h", "Print this help message.");
-
-    po::options_description hidden("Hidden options");
-    hidden.add_options()
-            ("a", po::value<int>(&variable_a)->default_value(0), "Variable A.")
-            ("b", po::value<int>(&variable_b)->default_value(0), "Variable B.");
-
-    po::positional_options_description p;
-    p.add("a", 1);
-    p.add("b", 1);
-
-    po::options_description all("All options");
-    all.add(visible).add(hidden);
-
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(all).positional(p).run(), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << "Usage:\n  add [a] [b]\n" << visible << std::endl;
-        return EXIT_SUCCESS;
+    while ( true ){
+        std::cout << getcwd(NULL, 0) << std::endl;
+        std::string cmd;
+        std::getline(std::cin, cmd);
+        const char* cpath = cmd.c_str();
+        cd(cpath);
+        std::cout << getcwd(NULL, 0) << std::endl;
+        return 0;
     }
 
-    int result = operations::add(variable_a, variable_b);
-    std::cout << result << std::endl;
-    return EXIT_SUCCESS;
 }
